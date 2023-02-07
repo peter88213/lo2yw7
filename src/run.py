17f61f5525b78d6f30f9ed69fd7f7b2815febaaa
peter88213 <peter.88213@t-online.de>
2023-02-07 11:06:07 +0100

    Provide a starter script (yet not working due to import problems in uno context).

---
+++ src/run.py
@@ -0,0 +1,23 @@
+import lo2yw7lib
+
+
+def export_yw():
+    lo2yw7lib.export_yw()
+
+
+def to_blank_lines():
+    lo2yw7lib.to_blank_lines()
+
+
+def indent_paragraphs():
+    lo2yw7lib.indent_paragraphs()
+
+
+def replace_bullets():
+    lo2yw7lib.replace_bullets()
+
+
+def is_anything_selected(oDoc):
+    return lo2yw7lib.is_anything_selected(oDoc)
+    
+to_blank_lines()
