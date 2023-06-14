c3abdbfcb1941172b8cd41e2ff6aff6677ca067f
peter88213 <peter.88213@t-online.de>
2023-06-14 11:20:31 +0200

    Prepare v1.3.1 based on PyWriter v12.13.2
    - Fix a bug where the scene content gets lost when splitting a scene at
    an appended chapter heading.

--- dist/lo2yw7-L-1.3.1/lo2yw7/yw_format.py
+++ dist/lo2yw7-L-1.3.1/lo2yw7/yw_format.py
@@ -1,6 +1,6 @@
 """Macros for formatting text. 
 
-Version 1.3.0
+Version 1.3.1
 Requires Python 3.6+
 Copyright (c) 2023 Peter Triesberger
 For further information see https://github.com/peter88213/lo2yw7
